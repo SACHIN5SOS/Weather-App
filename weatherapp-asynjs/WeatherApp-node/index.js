71e314cf2fa80bd961f84bc7aff4d9a26ce9730d
SACHIN5SOS <sachinb443@gmail.com>
2018-01-01 13:24:18 +0530

    callback errors

--- weatherapp-asynjs/WeatherApp-node/index.js
+++ weatherapp-asynjs/WeatherApp-node/index.js
@@ -14,13 +14,20 @@ const argv= yargs
 .argv;
 var encodedAddress= encodeURIComponent(argv.address);
 var url= 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress;
-console.log(url);
 request({
    url : 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress ,
    json:true
 },(error,response,body)=> {
     // console.log(JSON.stringify(body,undefined,2));  //to check all objects 
+    if(error){
+        console.log('Unable to connect with Google Server');
+    }
+    else if(body.status==='ZERO_RESULTS'){
+        console.log('Unable to find that address');
+    }
+    else if(body.status==='OK'){
     console.log(`Address:  ${body.results[0].formatted_address}`);
     console.log(`Longitude:  ${body.results[0].geometry.location.lng}`);
     console.log(`Latitude:  ${body.results[0].geometry.location.lat}`);
+}
  })
