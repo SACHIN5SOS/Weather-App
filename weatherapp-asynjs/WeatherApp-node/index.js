35f73d393c43c47165341ffff4a140fcaba07d34
SACHIN5SOS <sachinb443@gmail.com>
2018-01-01 12:16:25 +0530

    Fetch Longitude and Latitude

--- weatherapp-asynjs/WeatherApp-node/index.js
+++ weatherapp-asynjs/WeatherApp-node/index.js
@@ -3,5 +3,8 @@ request({
    url : 'https://maps.googleapis.com/maps/api/geocode/json?address=%20Bsf%20camp%20Indore' ,
    json:true
 },(error,response,body)=> {
-    console.log(body);
-})
+    // console.log(JSON.stringify(body,undefined,2));  //to check all objects 
+    console.log(`Address:  ${body.results[0].formatted_address}`);
+    console.log(`Longitude:  ${body.results[0].geometry.location.lng}`);
+    console.log(`Latitude:  ${body.results[0].geometry.location.lat}`);
+ })
