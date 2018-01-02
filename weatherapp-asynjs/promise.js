1b2751db52d2c3bf59ab80967fae8cb7abc704fa
SACHIN5SOS <sachinb443@gmail.com>
2018-01-02 17:47:40 +0530

    Promises chaining

--- weatherapp-asynjs/promise.js
+++ weatherapp-asynjs/promise.js
@@ -1,4 +1,32 @@
-var somePromise = new Promise((resolve,reject)=>{
+var asyncPromise= (a,b) => {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(typeof a === 'number' && typeof b==='number'){
+                resolve(a+b);
+       
+               }
+               else{
+                   reject('Both input are not Numbers');
+               }    
+        }, 2000);
+        
+    })
+}
+
+asyncPromise(5,6).then((message)=>{
+    console.log(`Result: ${message}`);
+    asyncPromise(message,33);
+
+}).then((message)=>{
+    console.log(`Result: ${message}`);
+
+}).catch((errorMessage)=>{    //to catch error in promises
+    console.log(errorMessage);
+})
+
+
+
+/* var somePromise = new Promise((resolve,reject)=>{
     setTimeout(() => {
         // resolve('Hey, It worked successfully');
         reject('Sorry, an error occurred');    
@@ -10,4 +38,4 @@ somePromise.then((message)=>{           //First argument when Pormise work succe
     console.log(message);
 },(errorMessage)=>{
     console.log(errorMessage);
-})
+}) */
